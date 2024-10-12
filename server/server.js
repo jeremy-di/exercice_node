668bb594e44bc5d90dfdd8149184ea3d72516734
jeremy-di <moinet.jeremy@gmail.com>
2024-10-12 22:57:49 +0200

    api finalisée

--- server/server.js
+++ server/server.js
@@ -26,20 +26,25 @@ const users = JSON.parse(fs.readFileSync('./data/users.json', 'utf-8'));
 // Mise en place de la route
 // ! Je mets en place la route qui va gérer la connexion en gros et qui sera / login comme : http://localhost:8000/login
 app.post('/login', async ( req, res ) => {
+    // ! Les constantes login et password contiennent le body de la requète (Ce qui sera rentré dans le client)
     const { login, password } = req.body;
+    // ! La constante user va identifier le champs login comme étant un login
     const user = users.find(u => u.login === login)
-
+    // ! Si pas d'utilisateur trouvé dans le fichier json, on retourne un message
     if ( !user ) {
         return res.status(401).json({ error : 'Login ou mot de passe incorrect' });
     }
-
+    
+    // ! La constante va stocké les informations du mot de passe et bcrypt va comparé le mot de passe entre coté client avec celui coté fichier json
     const mdpValide = await bcrypt.compare(password, user.hashed_password)
 
+    // ! Si mot de passe non valide alors on affiche un message
     if ( !mdpValide ) {
         return res.status(401).json({ error : 'Login ou mot de passe incorrect' });
     }
 
     // Reception des données coté client
+    // ! Si tout fonctionne, on renvoi les données au client en version json
     res.json({
         msg : 'Bravo !!! Connexion réussie',
         data : {
@@ -48,6 +53,7 @@ app.post('/login', async ( req, res ) => {
     });
 });
 
+// ! On écoute l'api sur le port 8000
 app.listen(port, () => {
     console.log(`Serveur démarré sur le port ${port}`);
 });
